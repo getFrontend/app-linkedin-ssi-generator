da946bb20a62cc25e146baaa6b3af6b3bba66db5
Sergey UP <sergey10goals@gmail.com>
2024-09-14 23:22:10 +0300

    added target image

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,8 @@
 export const profileImg =
   "https://media.licdn.com/dms/image/v2/D4D03AQGD0ztaweCjHw/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1699791670268?e=1731542400&v=beta&t=EB-xa4MqCdq6xG_SdPaSyAS_ivoBhLf-ArEzAW9L5D4";
 
+export const targetImg = "/assets/images/target.svg";
+
 export const componentNames = [
   "Establish your professional brand",
   "Find the right people",
